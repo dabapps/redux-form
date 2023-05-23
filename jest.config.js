80bd2a1780d9ef37e4d087b1c45e3d58bd09c96e
Jake 'Sid' Smith <hello@jakesidsmith.com>
2023-05-23 15:19:33 +0100

    Revert to original babel config and previous jest version to run babel 6

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 const config = {
   testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/lib/', '/src/__tests__/test-utils.js'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/lib/',
+    '/src/__tests__/test-utils.js',
+  ],
 };
 
 module.exports = config;
