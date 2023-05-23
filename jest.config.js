03a11efafc0e9f5e734f8d3ff7b0e3a06b490b40
Jake 'Sid' Smith <hello@jakesidsmith.com>
2023-05-23 15:01:23 +0100

    Update test ignore patterns

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 const config = {
   testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '__tests__/test-utils.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/lib/', '/src/__tests__/test-utils.js'],
 };
 
 module.exports = config;
